67a126a7a9189411539ccc98a4ae5d9dbd47ad69
thequang-ntq-admin <thequang012004@gmail.com>
2025-03-21 23:56:37 +0700

    Initialize

---
+++ springboot_backend/src/test/java/_22T1020362/SpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package _22T1020362;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
